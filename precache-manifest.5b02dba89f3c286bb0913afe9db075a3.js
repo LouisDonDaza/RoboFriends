c9c7bd6bcdb434d3d55b331b7f29ac9fbd3d0cee
Louis Daza <louisdaza@hotmail.com>
2018-12-05 19:00:15 -0800

    Updates

---
+++ precache-manifest.5b02dba89f3c286bb0913afe9db075a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LouisDonDaza/RoboFriends/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9b9aee92f2306c362f86",
+    "url": "/LouisDonDaza/RoboFriends/static/js/runtime~main.9b9aee92.js"
+  },
+  {
+    "revision": "5883c15b21220461fb4b",
+    "url": "/LouisDonDaza/RoboFriends/static/js/main.5883c15b.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/LouisDonDaza/RoboFriends/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "5883c15b21220461fb4b",
+    "url": "/LouisDonDaza/RoboFriends/static/css/main.226892c5.chunk.css"
+  },
+  {
+    "revision": "2d0e8929c70e90f1d10e243a318466d8",
+    "url": "/LouisDonDaza/RoboFriends/index.html"
+  }
+];
